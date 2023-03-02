4f3d8a4e345960395247fb5eb35b2954ad6ed830
smartercow <smartercow@gmail.com>
2023-03-02 13:10:41 +0100

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,11 @@ import Script from 'next/script';
 export default function Document() {
   return (
     <Html>
-      <head>
-        <title>Spices</title>
-        <Script id='show-banner' strategy='afterInteractive'>
+      <Head />
+      <body className={`${isDevelopment ? 'debug-screens' : ''}`}>
+        <Main />
+        <NextScript />
+        <Script>
           {`  var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
@@ -20,11 +22,6 @@ export default function Document() {
     g.async=true; g.src='//cdn.matomo.cloud/spicesstorevercelapp.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
   })();`}
         </Script>
-      </head>
-
-      <body className={`${isDevelopment ? 'debug-screens' : ''}`}>
-        <Main />
-        <NextScript />
       </body>
     </Html>
   );
